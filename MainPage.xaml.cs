180b89a4ce31564b2e5077b7a3da5aba9977df19
Marco van den Hout <hilvarenbeek@gmail.com>
2021-11-29 14:36:02 +0100

    testing xamarin forms xaml

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -16,9 +16,9 @@ namespace MauiMac
 		private void OnCounterClicked(object sender, EventArgs e)
 		{
 			count++;
-			CounterLabel.Text = $"Current count: {count}";
+			//CounterLabel.Text = $"Current count: {count}";
 
-			SemanticScreenReader.Announce(CounterLabel.Text);
+			//SemanticScreenReader.Announce(CounterLabel.Text);
 		}
 	}
 }
